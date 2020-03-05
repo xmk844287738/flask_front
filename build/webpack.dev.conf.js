1e3dfefde1a1cbc53fb5a41d4c85e11c0eedfa34
小麦客 <49434432+xmk844287738@users.noreply.github.com>
2020-03-05 11:31:34 +0800

    microblog_前端
    microblog_前端

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -45,12 +45,14 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     }
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env'),
-       $: 'jquery',
-       jquery: 'jquery',
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
       'window.jQuery': 'jquery',
-       jQuery: 'jquery'
+      jQuery: 'jquery'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': require('../config/dev.env')
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
